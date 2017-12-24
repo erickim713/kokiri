162b021bbd9603a5fe996e7c5861d1677c445ab4
erickim713 <erickim713@gmail.com>
2017-12-24 19:57:21 +0900

    kokiri~

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+// global variables
+let broswerWidth, browserHeight
+
+
+window.onload = ()=>{
+    console.log('window has been loaded');
+    
+    
+    browserHeight = window.innerHeight;
+    broswerWidth = window.innerWidth;
+    
+}
+
+$(document).ready(()=>{
+	$("#navi").hide();
+	$(window).scroll(function(){                          
+		if ($(this).scrollTop() > (browserHeight / 4)) {
+			$('#navi').fadeIn(500);
+		} else {
+			$('#navi').fadeOut(500);
+		}
+	});
+    //change the height of the cover background
+	$("#title").css('height', browserHeight);
+	let clock = new Vue({
+		el: '#time',
+		data: {
+			currentTime: '',
+			date: ''
+		}
+	});
+	let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+	setInterval(updateTime, 1000);
+	updateTime();
+
+	// time update per second update
+	function updateTime() {
+		let currentTime = new Date();
+		clock.currentTime = 
+		fillUpwithZerios(currentTime.getHours(), 2) + 
+		':' + fillUpwithZerios(currentTime.getMinutes(), 2) + 
+		':' + fillUpwithZerios(currentTime.getSeconds(), 2);
+		clock.date = 
+		fillUpwithZerios(currentTime.getFullYear(), 4) + 
+		'-' + fillUpwithZerios(currentTime.getMonth()+1, 2) + 
+		'-' + fillUpwithZerios(currentTime.getDate(), 2) + 
+		' ' + week[currentTime.getDay()];
+		if(currentTime.getHours() >= 11 && currentTime.getHours()<=23){
+			$("#currentTime").addClass("opened").removeClass("closed");
+			$("#opened").addClass("selected").removeClass('non-selected');
+			$("#closed").removeClass("selected").addClass('non-selected');
+		}
+		else{
+			$("#currentTime").addClass("closed").removeClass("opened");
+			$("#closed").addClass("selected").removeClass('non-selected');
+			$("#opened").removeClass("selected").addClass('non-selected');
+		}
+	};
+	
+	function fillUpwithZerios(num, digit) {
+		let zero = '';
+		for(let i = 0; i < digit; i++) {
+			zero += '0';
+		}
+		return (zero + num).slice(-digit);
+	}
+
+
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - 56
+		}, 500);
+		return false;
+	});
+});
+
+
+
+
+
+
+
